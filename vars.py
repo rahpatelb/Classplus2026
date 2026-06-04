ff047002b70fee52f2812a6b9f6fb97cc2d60273
rahpatelb <75303925+rahpatelb@users.noreply.github.com>
2026-06-04 17:18:49 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "27433400"))
-API_HASH = environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "22447622"))
+API_HASH = environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8448062014:AAEcVEUYxdcZlaGQUyIpkF7x9x8ww7C9--M")
 
-OWNER = int(environ.get("OWNER", "6201066540"))
+OWNER = int(environ.get("OWNER", "777756062"))
 CREDIT = environ.get("CREDIT", "KANHA 𝘽𝙊𝙏𝙎")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '6201066540').split(',')
